eee183512a6276c44fb3831fa714ad752730727b
Dylouwu <diloulefilou@gmail.com>
2024-05-17 14:53:35 +0900

    added spawnpoints and modified stuff

--- Assets/Scripts/NetworkSpawn.cs
+++ Assets/Scripts/NetworkSpawn.cs
@@ -5,27 +5,37 @@ using System.Collections.Generic;
 
 public class NetworkSpawn : NetworkBehaviour
 {
-    public GameObject objectToSpawnPrefab;
+    public GameObject[] objectToSpawnPrefab;
+    private List<GameObject> spawnedObjects = new List<GameObject>();
 
     public override void OnNetworkSpawn()
     {
         if (IsServer)
         {
-            base.OnNetworkSpawn();
-            SpawnObject();
+            foreach(var obj in objectToSpawnPrefab)
+            {
+                SpawnObject(obj);
+            }
         }
     }
 
-    void SpawnObject()
+    void SpawnObject(GameObject obj)
     {
-        GameObject spawnedObject = Instantiate(objectToSpawnPrefab, transform.position, transform.rotation);
+        GameObject spawnedObject = Instantiate(obj, transform.position, transform.rotation);
+        spawnedObjects.Add(spawnedObject); // Add spawned object to the list
         spawnedObject.GetComponent<NetworkObject>().Spawn(); // Ensure the object is spawned on the network
     }
 
     // Update is called once per frame
     void Update()
     {
-        objectToSpawnPrefab.transform.position = transform.position;
-        objectToSpawnPrefab.transform.rotation = transform.rotation;
+        foreach (var spawnedObject in spawnedObjects)
+        {
+            if (spawnedObject != null) // Check if the spawned object still exists
+            {
+                spawnedObject.transform.position = transform.position;
+                spawnedObject.transform.rotation = transform.rotation;
+            }
+        }
     }
 }
